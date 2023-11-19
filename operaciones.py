036a80f4b9f6ae2036a5518fe02e72ddf58ce6b7
delafug-dev <adelafuentegarceso@gmail.com>
2023-11-19 14:08:08 +0100

    test de CI CD operaciones

--- operaciones.py
+++ operaciones.py
@@ -1,7 +1,19 @@
 def suma(a, b):
     return a + b
 
+def resta(a, b):
+    return a - b
+
+def multiplicacion(a, b):
+    return a * b
+
+def division(a, b):
+    if b == 0:
+        raise ValueError("No se puede dividir por cero")
+    return a / b
+
 if __name__ == "__main__":
-    print(suma(1, 2))
-    print(suma(3, 4))
-    print(suma(5, 6))
+    print(suma(3, 2))
+    print(resta(5, 4))
+    print(multiplicacion(5, 6))
+    print(division(10, 2))
--- test_operaciones.py
+++ test_operaciones.py
@@ -5,10 +5,22 @@ class TestOperaciones(unittest.TestCase):
     def test_suma(self):
         self.assertEqual(suma(1,2), 3)
         self.assertEqual(suma(2,2), 4)
+        self.assertEqual(suma(3,6), 9)
 
-    # def test_resta(self):
-    #     self.assertEqual(resta(1,2), -1)
-    #     self.assertEqual(resta(1,2), -2)
+    def test_resta(self):
+        self.assertEqual(resta(1,2), -1)
+        self.assertEqual(resta(6,6), 0)
+        self.assertEqual(resta(10,2), 8)
+
+    def test_multiplicacion(self):
+        self.assertEqual(multiplicacion(1,2), 2)
+        self.assertEqual(multiplicacion(2,2), 4)
+        self.assertEqual(multiplicacion(3,6), 18)
+
+    def test_division(self):
+        self.assertEqual(division(1,2), 0.5)
+        self.assertEqual(division(6,6), 1)
+        self.assertEqual(division(10,2), 5)
 
 if __name__ == '__main__':
     unittest.main()
